9f8920325b1533688bca2001480f0c4f94500d40
Pujitha886 <eumamadhavi9384@gmail.com>
2026-08-01 23:12:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let friendName = "Akka & My Sis<3";
+
+document.getElementById("name").innerHTML =
+"Dear " + friendName + " ❤️";
+
+function showWish(){
+
+document.getElementById("wish").innerHTML =
+"🌸 Happy Friendship Day, " +
+friendName +
+"! May our friendship last forever! 💖🤝";
+
+}
